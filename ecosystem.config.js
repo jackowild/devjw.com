8365938461e49212e9bf9b5d449acf99bed6f633
Jack Wild <jackwild123@gmail.com>
2017-01-15 22:10:58 +0000

    Updated host in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ec2-user',
-      host: '52.56.47.8',
+      host: 'ec2-52-56-47-88.eu-west-2.compute.amazonaws.com',
       key: 'C:\ssh\devjw.pem',
       ref: 'origin/master',
       repo: 'https://github.com/jackowild/devjw.com.git',
