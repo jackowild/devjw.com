1f88aa4169fb7cdf741ea846264ca129d38c6eed
Jack Wild <jackwild123@gmail.com>
2017-01-17 23:22:59 +0000

    Added old website and serve as static content. This is very old and will be replaced.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('Website under construction')
-})
-app.listen(3000, () => console.log('Server running on port 3000'))
+const express = require('express');
+const app = express();
+
+app.use(express.static('src'));
+app.listen(3000, () => console.log('Server running on port 3000'));
